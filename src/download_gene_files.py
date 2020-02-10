656eb888e61808ee4b4a86646935ffd1c243b678
Hirokazu Chiba <hchiba1@gmail.com>
2020-02-10 16:27:22 +0900

    remove spaces

--- src/download_gene_files.py
+++ src/download_gene_files.py
@@ -3,4 +3,4 @@ from functions import download
 server = 'ftp.ncbi.nlm.nih.gov'
 file = '/genomes/ASSEMBLY_REPORTS/assembly_summary_refseq.txt'
 
-download( server, file, 'assermbly_summary_refseq.txt' )
+download(server, file, 'assermbly_summary_refseq.txt')
