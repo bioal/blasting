b6873172ad8b225d0e4951ec303fe87cb528d18e
Satoshi Tanaka <satstnka@trans-it.jp>
2020-02-10 16:07:59 +0900

    adds the functionality to download the specified file via ftp.

---
+++ src/download_gene_files.py
@@ -0,0 +1,6 @@
+from functions import download
+
+server = 'ftp.ncbi.nlm.nih.gov'
+file = '/genomes/ASSEMBLY_REPORTS/assembly_summary_refseq.txt'
+
+download( server, file, 'assermbly_summary_refseq.txt' )
