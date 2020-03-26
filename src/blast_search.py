885a9d93efc2d9bff1482d0b9e2bdd5c939ad36e
Satoshi Tanaka <satstnka@gmail.com>
2020-03-26 15:46:49 +0900

    added -p option to make_database.py and blast_search.

--- src/blast_search.py
+++ src/blast_search.py
@@ -9,11 +9,14 @@ parser = OptionParser(sys.argv)
 result_folder = parser.get_option('o')
 list_file = parser.get_option('l')
 num = parser.get_option('n')
+command = parser.get_option('p')
+if command == None:
+    command = 'blastp'
 
 if result_folder == None or list_file == None:
     print('Usage: blast_search.py -o [output_folder] -l [list_file] -n [number of threads]')
-    print(' e.g., blast_search.py -o /opt/orthology/data/genome/blast -l databases.txt -n 4')
+    print(' e.g., blast_search.py -o /opt/orthology/data/genome/blast -l databases.txt -n 4 -p /usr/local/bin/blastp')
 else:
-    manager = BlastManager(result_folder, list_file, num, '/opt/packages/blast/ncbi-blast-2.10.0+-src/c++/ReleaseMT/bin/blastp')
+    manager = BlastManager(result_folder, list_file, num, command)
     manager.search()
 
