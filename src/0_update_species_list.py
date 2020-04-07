3432f890e41f30e3f034efb9845e5e02d2a1e828
Hirokazu Chiba <hchiba1@gmail.com>
2020-04-08 06:56:13 +0900

    update 0_update_species_list.py

--- src/0_update_species_list.py
+++ src/0_update_species_list.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
+import argparse
 import requests
+
+parser = argparse.ArgumentParser(description='Get the target species list in tsv format.')
+parser.add_argument('-o', '--outfile', default='species_list.tsv', help='Output file')
+args = parser.parse_args()
 
 response = requests.get('https://docs.google.com/spreadsheets/d/1-7FY-B_BpU72A045EeEuExea6FtMs8q-Urn9-R6-TWk/export?format=tsv')
 
-print(response.text)
+fp = open(args.outfile, 'w')
+
+fp.write(response.text)
+
+fp.close()
