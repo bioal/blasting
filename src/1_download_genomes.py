2127f069272570e7d7c550cb12306e88cfd90486
Hirokazu Chiba <hchiba1@gmail.com>
2020-04-08 01:39:47 +0900

    modify 1_download_genomes.py

--- src/1_download_genomes.py
+++ src/1_download_genomes.py
@@ -6,13 +6,17 @@ from classes.OptionParser import OptionParser
 
 parser = OptionParser(sys.argv)
 
-data_folder = parser.get_option('o')
 list_file = parser.get_option('l')
 
-if data_folder == None or list_file == None:
+data_folder = parser.get_option('o')
+if data_folder == None:
+    data_folder = 'genomes'
+
+if data_folder == None or len(sys.argv) != 1:
     print('Usage: download_gene_files.py -o [output_folder] -l [list_file]')
     print(' e.g., download_gene_files.py -o /opt/orthology/data/genome -l /opt/orthology/data/species_list.tsv')
 else:
+    list = sys.argv[0]
     downloader = GenomeDownloader(data_folder, list_file)
     downloader.download()
 
