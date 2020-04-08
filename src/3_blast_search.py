5f155f2f93f9c6cb37a92750ebefda531a597cf2
Hirokazu Chiba <hchiba1@gmail.com>
2020-04-09 03:55:26 +0900

    update blast_search

--- src/3_blast_search.py
+++ src/3_blast_search.py
@@ -3,11 +3,11 @@ import argparse
 from classes.BlastManager import BlastManager
 
 parser = argparse.ArgumentParser(description='BLAST search for the specified genomes.')
-parser.add_argument('db_list', help='List of dbs in tsv format')
-parser.add_argument('-n', '--threads', required=True, type=int, help='Number of threads to be used for BLAST')
+parser.add_argument('genomes_list', help='List of genomes in tsv format')
+parser.add_argument('-n', '--cores', required=True, type=int, help='Number of CPU cores to be used for BLAST')
 parser.add_argument('-p', '--program', default='blastp', help='Path to blastp command (default: blastp)')
 parser.add_argument('-o', '--outdir', default='blast', help='Output directory')
 args = parser.parse_args()
 
-manager = BlastManager(args.program, args.threads, args.db_list, args.outdir)
+manager = BlastManager(args.program, args.cores, args.genomes_list, args.outdir)
 manager.search()
--- src/classes/BlastManager.py
+++ src/classes/BlastManager.py
@@ -4,62 +4,48 @@ import subprocess
 import os
 
 class BlastManager:
-    # constructor
-    def __init__(self, command, num, db_list, output_folder):
+    def __init__(self, command, num, genome_list, output_folder):
         if not os.path.exists(output_folder):
             os.makedirs(output_folder) 
         self.output_folder = output_folder
-        self.genome_list = self.__get_genome_list(db_list)
+        self.genome_list = self.__read_genome_list(genome_list)
         self.semaphore = Semaphore(int(num))
         self.command = command
 
-    # get list
-    def __get_genome_list(self, db_list):
-        fp = open(db_list, 'r')
+    def __read_genome_list(self, genome_list):
+        fp = open(genome_list, 'r')
         list = []
         for line in fp:
             tokens = line.strip().split('\t')
             if len(tokens) >= 4:
-                id = tokens[0]
-                gcf_id = tokens[1]
-                species = tokens[2]
-                fasta_file = tokens[3]
-                genome = {'id':id, 'gcf_id':gcf_id, 'species':species, 'fasta_file':fasta_file}
-                list.append(genome)
+                list.append({'id':tokens[0], 'gcf_id':tokens[1], 'species':tokens[2], 'fasta_file':tokens[3]})
         fp.close()
         return list
 
-    # search
     def search(self):
-        human = None
+        human_genome = None
         for genome in self.genome_list:
             if int(genome['id']) == 1:
-                human = genome
+                human_genome = genome
 
         for genome in self.genome_list:
-            thread1 = Thread(target=self.__execute_blast, args=(genome, human))
+            thread1 = Thread(target=self.__execute_blast, args=(genome, human_genome))
             thread1.start()
 
-            if not genome == human:
-                thread2 = Thread(target=self.__execute_blast, args=(human, genome))
+            if not genome == human_genome:
+                thread2 = Thread(target=self.__execute_blast, args=(human_genome, genome))
                 thread2.start()
 
     def __execute_blast(self, genome1, genome2):
         with self.semaphore:
-            output_file = self.output_folder + '/' + genome1['id'] + '-' + genome2['id']
-            query = genome1['fasta_file']
-            db = 'db/' + genome2['id']
-
             command = [
                 self.command,
-                '-query', query,
-                '-db', db,
+                '-query', genome1['fasta_file'],
+                '-db', 'db/' + genome2['id'],
                 '-max_target_seqs', '1',
                 '-outfmt', '6',
-                '-out', output_file
+                '-out', self.output_folder + '/' + genome1['id'] + '-' + genome2['id']
             ]
-            command_line = ' '.join(command)
-            print('Exec: ' + command_line)
+            print('Exec: ' + ' '.join(command))
             process = subprocess.Popen(command)
             process.wait()
-
