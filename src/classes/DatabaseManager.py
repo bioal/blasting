1cbd2331ca466af255386a746913f99705930432
Hirokazu Chiba <hchiba1@gmail.com>
2020-04-08 14:04:11 +0900

    refactor src/classes/DatabaseManager.py

--- src/classes/DatabaseManager.py
+++ src/classes/DatabaseManager.py
@@ -3,26 +3,26 @@ import os
 
 class DatabaseManager:
     # constructor
-    def __init__(self, output_folder, list_file, command):
+    def __init__(self, output_folder, genome_file_list, command):
         if not os.path.exists(output_folder):
             os.makedirs(output_folder)
         self.output_folder = output_folder
-        self.list_file = self.__read_list_file(list_file)
+        self.genome_list = self.__read_genome_list(genome_file_list)
         self.command = command
 
 
     # read list file
-    def __read_list_file(self, list_file):
-        fp = open(list_file, 'r')
-        list = []
+    def __read_genome_list(self, genome_file_list):
+        genome_list = []
+        fp = open(genome_file_list, 'r')
         for line in fp:
             tokens = line.strip().split('\t')
             if len(tokens) >= 4:
                 genome = { 'id': tokens[0], 'gene_id': tokens[1], 'species': tokens[2], 'file': tokens[3] }
-                list.append(genome)
+                genome_list.append(genome)
         fp.close()
-        list.sort(key=lambda x: int(x['id']))
-        return list
+        genome_list.sort(key=lambda x: int(x['id']))
+        return genome_list
 
     # make database
     def make_database(self):
@@ -35,27 +35,27 @@ class DatabaseManager:
         if not os.path.exists(list_dir):
             os.makedirs(list_dir)
 
-        for genome in self.list_file:
+        for genome in self.genome_list:
             id = genome['id']
             file = genome['file']
             species = genome['species']
             gene_id = genome['gene_id']
 
-            list_file = list_dir + '/' + id + '.txt'
+            gene_list_file = list_dir + '/' + id + '.txt'
             database = database_dir + '/' + id
 
-            self.__make_list(file, list_file)
+            self.__make_list(file, gene_list_file)
             self.__make_db(file, database)
 
-            line = id + '\t' + gene_id + '\t' + species + '\t' + file + '\t' + list_file + '\t' + database + '\n'
+            line = id + '\t' + gene_id + '\t' + species + '\t' + file + '\t' + gene_list_file + '\t' + database + '\n'
             fp.write(line)
 
         fp.close()
 
     # make list file
-    def __make_list(self, faa_file, list_file):
+    def __make_list(self, faa_file, gene_list_file):
         in_fp = open(faa_file, 'r')
-        out_fp = open(list_file, 'w')
+        out_fp = open(gene_list_file, 'w')
 
         count = 1
         for line in in_fp:
