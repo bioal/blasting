54a3095cfb4ec79d19beaf87ffcabb2fa0aa176b
Hirokazu Chiba <hchiba1@gmail.com>
2020-04-08 20:58:53 +0900

    refactor src/classes/BlastManager.py

--- src/classes/BlastManager.py
+++ src/classes/BlastManager.py
@@ -5,29 +5,29 @@ import os
 
 class BlastManager:
     # constructor
-    def __init__(self, output_folder, list_file, num, command):
+    def __init__(self, output_folder, db_list, num, command):
         if not os.path.exists(output_folder):
             os.makedirs(output_folder) 
         self.output_folder = output_folder
-        self.genome_list = self.__get_list(list_file)
+        self.genome_list = self.__get_genome_list(db_list)
         self.semaphore = Semaphore(int(num))
         self.command = command
 
     # get list
-    def __get_list(self, list_file):
-        fp = open(list_file, 'r')
+    def __get_genome_list(self, db_list):
+        fp = open(db_list, 'r')
         list = []
         for line in fp:
             tokens = line.strip().split('\t')
             if len(tokens) >= 6:
                 id = tokens[0]
-                gene_id = tokens[1]
+                gcf_id = tokens[1]
                 species = tokens[2]
                 faa_file = tokens[3]
-                list_file = tokens[4]
-                database = tokens[5]
+                genes_file = tokens[4]
+                db = tokens[5]
                 
-                genome = {'id':id, 'gene_id':gene_id, 'species':species, 'faa_file':faa_file, 'list_file': list_file, 'database': database}
+                genome = {'id':id, 'gcf_id':gcf_id, 'species':species, 'faa_file':faa_file, 'genes_file': genes_file, 'db': db}
                 list.append(genome)
         fp.close()
         return list
@@ -52,13 +52,13 @@ class BlastManager:
     def __execute_blast(self, genome1, genome2):
         with self.semaphore:
             output_file = self.output_folder + '/' + genome1['id'] + '-' + genome2['id'] + '.txt'
-            database = genome2['database']
+            db = genome2['db']
             query = genome1['faa_file']
 
             command = [
                 self.command,
                 '-db',
-                database,
+                db,
                 '-query',
                 query,
                 '-max_target_seqs', '1',
