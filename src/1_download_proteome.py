6592142e23d2c51036da38727c4a171d8bb30872
Hirokazu Chiba <hchiba1@gmail.com>
2020-04-21 15:27:09 +0900

    fix 1_download_proteome.py

--- src/1_download_proteome.py
+++ src/1_download_proteome.py
@@ -4,7 +4,7 @@ from classes.ProteomeDownloader import ProteomeDownloader
 
 parser = argparse.ArgumentParser(description='Download proteomes from UniProt, according to the species list in tsv format.')
 parser.add_argument('species_list', help='Species list in tsv format')
-parser.add_argument('-o', '--outdir', default='genome', help='Output directory')
+parser.add_argument('-o', '--outdir', default='proteome', help='Output directory')
 parser.add_argument('-d', '--debug', action='store_true', help='For debug: do not download the genomes')
 args = parser.parse_args()
 
