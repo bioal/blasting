a058159c3d5dbd015da8281039bd147f233547ad
Hirokazu Chiba <hchiba1@gmail.com>
2020-04-21 15:20:29 +0900

    fix classes/ProteomeManager.py

--- src/classes/ProteomeManager.py
+++ src/classes/ProteomeManager.py
@@ -2,41 +2,41 @@ import subprocess
 import os
 
 class DataManager:
-    def __init__(self, genome_file_list, command):
-        self.genes_dir = 'genes'
+    def __init__(self, proteome_file_list, command):
+        self.proteins_dir = 'proteins'
         self.db_dir = 'db'
-        if not os.path.exists(self.genes_dir):
-            os.makedirs(self.genes_dir)
+        if not os.path.exists(self.proteins_dir):
+            os.makedirs(self.proteins_dir)
         if not os.path.exists(self.db_dir):
             os.makedirs(self.db_dir)
-        self.genome_list = self.__read_genome_list(genome_file_list)
+        self.proteome_list = self.__read_proteome_list(proteome_file_list)
         self.command = command
 
-    def __read_genome_list(self, genome_file_list):
-        genome_list = []
-        fp = open(genome_file_list, 'r')
+    def __read_proteome_list(self, proteome_file_list):
+        proteome_list = []
+        fp = open(proteome_file_list, 'r')
         for line in fp:
             tokens = line.strip().split('\t')
             if len(tokens) >= 2:
-                genome = { 'id': tokens[0], 'fasta_file': tokens[1] }
-                genome_list.append(genome)
+                proteome = { 'id': tokens[0], 'fasta_file': tokens[1] }
+                proteome_list.append(proteome)
         fp.close()
-        genome_list.sort(key=lambda x: int(x['id']))
-        return genome_list
+        proteome_list.sort(key=lambda x: int(x['id']))
+        return proteome_list
 
     def preprocess(self):
-        for genome in self.genome_list:
-            id = genome['id']
-            fasta_file = genome['fasta_file']
-            genes_list = self.genes_dir + '/' + id
+        for proteome in self.proteome_list:
+            id = proteome['id']
+            fasta_file = proteome['fasta_file']
+            proteins_list = self.proteins_dir + '/' + id
             db = self.db_dir + '/' + id
 
-            self.__make_genes_list(fasta_file, genes_list)
+            self.__make_proteins_list(fasta_file, proteins_list)
             self.__make_db(fasta_file, db)
 
-    def __make_genes_list(self, fasta_file, genes_list):
+    def __make_proteins_list(self, fasta_file, proteins_list):
         in_fp = open(fasta_file, 'r')
-        out_fp = open(genes_list, 'w')
+        out_fp = open(proteins_list, 'w')
 
         count = 1
         for line in in_fp:
