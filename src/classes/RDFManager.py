aeb0db54356ffc2fd95ea2dacf8b8580f6d33159
Hirokazu Chiba <hchiba1@gmail.com>
2020-05-11 07:37:11 +0900

    modiry RDFManager to rdfize proteome

--- src/classes/RDFManager.py
+++ src/classes/RDFManager.py
@@ -36,3 +36,40 @@ class RDFManager:
         #     else:
         #         result_fp.write(id + '\t' + file_obtained[id] + '\n');
         # result_fp.close()
+
+    def rdfize_proteome(self):
+        print('@prefix dct: <http://purl.org/dc/terms/> .')
+        print('@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .')
+        print('@prefix proteome: <http://purl.uniprot.org/proteomes/> .')
+        print('@prefix up: <http://purl.uniprot.org/core/> .')
+        print('')
+        file_obtained = {}
+        fp = open(self.summary_file, 'r', encoding='UTF-8')
+        line = fp.readline()
+        while line:
+            line = line.strip()
+            tokens = line.split('\t')
+            if line.startswith('UP000') and len(tokens) >= 8:
+                gcf_id = tokens[0]
+                taxid = tokens[1]
+                oscode = tokens[2]
+                proteins = tokens[4]
+                isoforms = tokens[5]
+                species = tokens[7]
+                # print(f'proteome:{gcf_id} mbgd:inTaxon taxid:{taxid} ;')
+                print(f'proteome:{gcf_id}')
+                print(f'    dct:identifier "{gcf_id}" ;')
+                print(f'    rdfs:label "{species}" ;')
+                print(f'    up:oscode "{oscode}" ;')
+                print(f'    up:proteins {proteins} ;')
+                print(f'    up:isoforms {isoforms} .')
+                print('')
+                # url = 'ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Eukaryota/' + gcf_id + '_' + taxid + '.fasta.gz'
+                # id = self.species_hash.get(species) or \
+                #      self.taxid_hash.get(taxid) or \
+                #      self.taxid_hash.get(species_taxid)
+                # if id is not None and file_obtained.get(id) is None:
+                #     gcf_file = self.__download_gcf_file(url, debug)
+                #     file_obtained[id] = gcf_file
+            line = fp.readline()
+        fp.close()
--- src/make_rdf.py
+++ src/make_rdf.py
@@ -4,8 +4,12 @@ from classes.RDFManager import RDFManager
 
 parser = argparse.ArgumentParser(description='Make RDF')
 parser.add_argument('summary_file', help='Summary file')
+parser.add_argument('-p', '--proteome', action='store_true', help='For proteome')
 # parser.add_argument('-p', '--program', default='makeblastdb', help='Path to makeblsastdb command (default: makeblastdb)')
 args = parser.parse_args()
 
 manager = RDFManager(args.summary_file)
-manager.rdfize_genome()
+if args.proteome:
+    manager.rdfize_proteome()
+else:
+    manager.rdfize_genome()
