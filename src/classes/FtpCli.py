67bd948a37c9ec25c53bd311c89f36fabb4b43b4
Hirokazu Chiba <hchiba1@gmail.com>
2020-06-12 19:33:33 +0900

    update classes/FtpCli.py

--- src/classes/FtpCli.py
+++ src/classes/FtpCli.py
@@ -9,22 +9,6 @@ class FtpCli:
     def __init__(self, server):
         self.ftp = ftplib.FTP(server, 'anonymous', '')
 
-    def check_up_to_date(self, path):
-        remote_size = self.ftp.size(path)
-        remote_date = self.__get_remote_datetime(path)
-        remote_utime = remote_date.timestamp()
-        # print(path, remote_size, remote_date, remote_utime, sep='\t', flush=True)
-        local_name = os.path.basename(path)
-        if os.path.exists(local_name):
-            local_size = os.path.getsize(local_name)
-            local_utime = os.path.getmtime(local_name)
-            local_datetime = datetime.datetime.fromtimestamp(local_utime)
-            # print(local_name, local_size, local_datetime, local_utime)
-            # os.utime(local_name, (remote_utime, remote_utime))
-            if local_size == remote_size and local_datetime >= remote_date:
-                return True
-        return False
-
     def is_up_to_date(self, path, local_name):
         remote_size = self.ftp.size(path)
         remote_date = self.__get_remote_datetime(path)
