9b155aea8a334c6ccabbad981c5029923fb20e5f
Hirokazu Chiba <hchiba1@gmail.com>
2020-12-08 04:30:16 +0900

    add src/3_blast_all_vs_all.py

---
+++ src/3_blast_all_vs_all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import argparse
+from classes.BlastManager import BlastManager
+
+parser = argparse.ArgumentParser(description='All aginst all BLAST for the specified organisms.')
+parser.add_argument('organism_list', help='List of organisms in tsv format')
+parser.add_argument('-n', '--cores', required=True, type=int, help='Number of CPU cores to be used for BLAST')
+parser.add_argument('-p', '--program', default='blastp', help='Path to blastp command (default: blastp)')
+parser.add_argument('-o', '--outdir', default='blast', help='Output directory')
+args = parser.parse_args()
+
+manager = BlastManager(args.program, args.cores, args.organism_list, args.outdir)
+manager.all_vs_all()
