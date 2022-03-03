d0beab571b411e060738c9566a4212faf517ee78
Hirokazu Chiba <hchiba1@gmail.com>
2022-03-03 14:41:28 +0900

    Modify bin/classes/SpeciesManager.py to use regex to recognize header line

--- bin/classes/SpeciesManager.py
+++ bin/classes/SpeciesManager.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 class SpeciesManager:
     def __init__(self, species_list):
@@ -10,12 +11,11 @@ class SpeciesManager:
         for line in fp:
             line = line.rstrip('\r\n')
             fields = line.split('\t')
-            if not fields[0].isdigit:
-                # skip header line
-                continue
-            self.ids[fields[0]] = line
-            if len(fields) > 2:
-                self.species[fields[2]] = fields[0]
-            if len(fields) > 1:
-                self.taxids[fields[1]] = fields[0]
+            # skip header line
+            if re.match(r'^[1-9][0-9]*$', fields[0]):
+                self.ids[fields[0]] = line
+                if len(fields) > 2:
+                    self.species[fields[2]] = fields[0]
+                if len(fields) > 1:
+                    self.taxids[fields[1]] = fields[0]
         fp.close()
