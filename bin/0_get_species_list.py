ec2a8bfe1a426cba7e20ed61243aa57f29766a5f
Hirokazu Chiba <hchiba1@gmail.com>
2022-03-04 08:50:28 +0900

    Update bin/0_get_species_list.py help message

--- bin/0_get_species_list.py
+++ bin/0_get_species_list.py
@@ -3,7 +3,7 @@ import argparse
 import requests
 
 parser = argparse.ArgumentParser(description='Get the target species list in tsv format.')
-parser.add_argument('-o', '--outfile', help='Output file')
+parser.add_argument('-o', '--outfile', help='Output file (STDOUT if not specified)')
 args = parser.parse_args()
 
 response = requests.get('https://docs.google.com/spreadsheets/d/1-7FY-B_BpU72A045EeEuExea6FtMs8q-Urn9-R6-TWk/export?format=tsv')
