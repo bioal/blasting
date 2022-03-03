1429fcf2faa3ba5ea8c651b79b5d8ef2e3d13da1
Hirokazu Chiba <hchiba1@gmail.com>
2022-03-03 12:57:17 +0900

    Add comments in bin/classes/SpeciesManager.py

--- bin/classes/SpeciesManager.py
+++ bin/classes/SpeciesManager.py
@@ -2,6 +2,9 @@ import sys
 
 class SpeciesManager:
     def __init__(self, species_list):
+        # [0] no
+        # [1] species
+        # [6] taxid
         self.ids, self.taxids, self.species = {}, {}, {}
         fp = open(species_list, 'r')
         for line in fp:
