7ec5d69e5d210518de7bf561d82eb4aa09a77868
Hirokazu Chiba <hchiba1@gmail.com>
2022-03-03 12:48:04 +0900

    Refactor bin/classes/SpeciesManager.py

--- bin/classes/SpeciesManager.py
+++ bin/classes/SpeciesManager.py
@@ -2,21 +2,17 @@ import sys
 
 class SpeciesManager:
     def __init__(self, species_list):
-        self.species_list = species_list
-
-    def get_hash(self):
-        id_hash, taxid_hash, species_hash = {}, {}, {}
-        fp = open(self.species_list, 'r')
+        self.ids, self.taxids, self.species = {}, {}, {}
+        fp = open(species_list, 'r')
         for line in fp:
             line = line.rstrip('\r\n')
             fields = line.split('\t')
             if not fields[0].isdigit:
                 # skip header line
                 continue
-            id_hash[fields[0]] = line
+            self.ids[fields[0]] = line
             if len(fields) >= 2:
-                species_hash[fields[1]] = fields[0]
+                self.species[fields[1]] = fields[0]
             if len(fields) >= 7:
-                taxid_hash[fields[6]] = fields[0]
+                self.taxids[fields[6]] = fields[0]
         fp.close()
-        return id_hash, taxid_hash, species_hash
