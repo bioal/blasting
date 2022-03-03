5d6305741f5d9fbe05b4e8ffd07b99bb505bd85c
Hirokazu Chiba <hchiba1@gmail.com>
2022-03-03 13:17:27 +0900

    Modify bin/classes/SpeciesManager.py to read first 3 columns

--- bin/classes/SpeciesManager.py
+++ bin/classes/SpeciesManager.py
@@ -3,8 +3,8 @@ import sys
 class SpeciesManager:
     def __init__(self, species_list):
         # [0] no
-        # [1] species
-        # [6] taxid
+        # [1] taxid
+        # [2] name
         self.ids, self.taxids, self.species = {}, {}, {}
         fp = open(species_list, 'r')
         for line in fp:
@@ -14,8 +14,8 @@ class SpeciesManager:
                 # skip header line
                 continue
             self.ids[fields[0]] = line
-            if len(fields) >= 2:
-                self.species[fields[1]] = fields[0]
-            if len(fields) >= 7:
-                self.taxids[fields[6]] = fields[0]
+            if len(fields) > 2:
+                self.species[fields[2]] = fields[0]
+            if len(fields) > 1:
+                self.taxids[fields[1]] = fields[0]
         fp.close()
