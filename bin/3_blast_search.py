6e2d822facf21bf1cd781776801f51f45dd0ae61
Hirokazu Chiba <hchiba1@gmail.com>
2022-03-04 09:20:56 +0900

    Update bin/3_blast_search.py to use data/blast

--- bin/3_blast_search.py
+++ bin/3_blast_search.py
@@ -6,8 +6,8 @@ parser = argparse.ArgumentParser(description='BLAST search for the specified gen
 parser.add_argument('genome_list', help='List of genomes in tsv format')
 parser.add_argument('-n', '--cores', required=True, type=int, help='Number of CPU cores to be used for BLAST')
 parser.add_argument('-p', '--program', default='blastp', help='Path to blastp command (default: blastp)')
-parser.add_argument('-o', '--outdir', default='blast', help='Output directory')
+parser.add_argument('-o', '--outdir', default='data/blast', help='Output directory')
 args = parser.parse_args()
 
-manager = BlastManager(args.program, args.cores, args.genome_list, args.outdir)
+manager = BlastManager(args.program, args.cores, args.genome_list, 'data/blastdb', args.outdir)
 manager.search()
