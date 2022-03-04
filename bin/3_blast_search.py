ee59f4b3edc0878000e2036fd7c106246152aaf3
Hirokazu Chiba <hchiba1@gmail.com>
2022-03-04 10:15:05 +0900

    Refactor 3_blast_search.py to use pairs of organisms

--- bin/3_blast_search.py
+++ bin/3_blast_search.py
@@ -2,12 +2,12 @@
 import argparse
 from classes.BlastManager import BlastManager
 
-parser = argparse.ArgumentParser(description='BLAST search for the specified genomes.')
-parser.add_argument('genome_list', help='List of genomes in tsv format')
+parser = argparse.ArgumentParser(description='BLAST search for pairs of the specified organisms.')
+parser.add_argument('organism_list', help='List of organisms in tsv format')
 parser.add_argument('-n', '--cores', required=True, type=int, help='Number of CPU cores to be used for BLAST')
 parser.add_argument('-p', '--program', default='blastp', help='Path to blastp command (default: blastp)')
 parser.add_argument('-o', '--outdir', default='data/blast', help='Output directory')
 args = parser.parse_args()
 
-manager = BlastManager(args.program, args.cores, args.genome_list, 'data/blastdb', args.outdir)
+manager = BlastManager(args.program, args.cores, args.organism_list, 'data/blastdb', args.outdir)
 manager.search()
