d2ce76b7db82743aafdee9ed34d209c326fc3b56
Hirokazu Chiba <hchiba1@gmail.com>
2022-03-25 10:50:47 +0900

    Refactor bin/classes/BlastManager.py

--- bin/classes/BlastManager.py
+++ bin/classes/BlastManager.py
@@ -5,17 +5,17 @@ import os
 import sys
 
 class BlastManager:
-    def __init__(self, command, num, genome_list, db_dir, out_dir):
+    def __init__(self, command, num, organism_list, db_dir, out_dir):
         self.command = command
         self.semaphore = Semaphore(int(num))
-        self.genome_list = self.__read_genome_list(genome_list)
+        self.organisms = self.__read_organism_list(organism_list)
         self.db_dir = db_dir
         if not os.path.exists(out_dir):
             os.makedirs(out_dir) 
         self.out_dir = out_dir
 
-    def __read_genome_list(self, genome_list):
-        fp = open(genome_list, 'r')
+    def __read_organism_list(self, organism_list):
+        fp = open(organism_list, 'r')
         list = []
         for line in fp:
             tokens = line.strip().split('\t')
@@ -25,35 +25,35 @@ class BlastManager:
         return list
 
     def all_vs_all(self):
-        for genome1 in self.genome_list:
-            for genome2 in self.genome_list:
-                thread1 = Thread(target=self.__execute_blast, args=(genome1, genome2))
+        for org1 in self.organisms:
+            for org2 in self.organisms:
+                thread1 = Thread(target=self.__execute_blast, args=(org1, org2))
                 thread1.start()
-                print(genome1['id'] + '-' + genome2['id'], flush=True)
+                print('Queued ' + org1['id'] + '-' + org2['id'], flush=True)
 
     def search(self):
-        human_genome = None
-        for genome in self.genome_list:
-            if int(genome['id']) == 1:
-                human_genome = genome
+        human_info = None
+        for org in self.organisms:
+            if int(org['id']) == 1:
+                human_info = org
 
-        for genome in self.genome_list:
-            thread1 = Thread(target=self.__execute_blast, args=(genome, human_genome))
+        for org in self.organisms:
+            thread1 = Thread(target=self.__execute_blast, args=(org, human_info))
             thread1.start()
 
-            if not genome == human_genome:
-                thread2 = Thread(target=self.__execute_blast, args=(human_genome, genome))
+            if not org == human_info:
+                thread2 = Thread(target=self.__execute_blast, args=(human_info, org))
                 thread2.start()
 
-    def __execute_blast(self, genome1, genome2):
+    def __execute_blast(self, org1, org2):
         with self.semaphore:
-            query_file = genome1['fasta_file']
-            db_file = self.db_dir + '/' + genome2['id']
-            out_file = self.out_dir + '/' + genome1['id'] + '-' + genome2['id'] + '.out'
-            log_file = self.out_dir + '/' + genome1['id'] + '-' + genome2['id'] + '.start'
-            err_file = self.out_dir + '/' + genome1['id'] + '-' + genome2['id'] + '.end'
-            if os.path.exists(err_file):
-                print(f'Found {err_file}, skip', file=sys.stderr)
+            query_file = org1['fasta_file']
+            db_file = self.db_dir + '/' + org2['id']
+            out_file = self.out_dir + '/' + org1['id'] + '-' + org2['id'] + '.out'
+            log_file = self.out_dir + '/' + org1['id'] + '-' + org2['id'] + '.log'
+            end_file = self.out_dir + '/' + org1['id'] + '-' + org2['id'] + '.end'
+            if os.path.exists(end_file):
+                print(f'Found {end_file}, skip', file=sys.stderr)
                 return
             command = [ self.command, '-query', query_file, '-db', db_file, '-out', out_file,
                 # '-max_target_seqs', '1',
@@ -66,6 +66,6 @@ class BlastManager:
                 ret = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 log_fp.write(ret.stdout.decode())
                 log_fp.flush()
-                with open(err_file, 'w') as err_fp:
-                    err_fp.write(ret.stderr.decode())
-                    err_fp.flush()
+                with open(end_file, 'w') as end_fp:
+                    end_fp.write(ret.stderr.decode())
+                    end_fp.flush()
