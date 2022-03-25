3cb5bdc91b4e0be2dff7e9d965b512b4bd0c7d0b
Hirokazu Chiba <hchiba1@gmail.com>
2022-03-25 13:35:16 +0900

    Update bin/4_bbh_all_vs_all.py

--- bin/4_bbh_all_vs_all.py
+++ bin/4_bbh_all_vs_all.py
@@ -5,7 +5,7 @@ from classes.BBH import BBH
 parser = argparse.ArgumentParser(description='Summarize BLAST search resutls.')
 parser.add_argument('organism_list', help='List of organisms in tsv format')
 parser.add_argument('--blastdir', default='blast', help='BLAST results directory')
-parser.add_argument('-o', '--outdir', default='matrix', help='Output directory')
+parser.add_argument('-o', '--outdir', default='bbh', help='Output directory')
 args = parser.parse_args()
 
 manager = BBH(args.blastdir, args.outdir, args.organism_list)
