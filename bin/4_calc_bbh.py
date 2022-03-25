16e299ac0698ff918c4fff14f56ec1002a1fb950
Hirokazu Chiba <hchiba1@gmail.com>
2022-03-25 12:04:47 +0900

    Update bin/4_calc_bbh.py to unset default organism_list

--- bin/4_calc_bbh.py
+++ bin/4_calc_bbh.py
@@ -3,7 +3,7 @@ import argparse
 from classes.ResultManager import ResultManager
 
 parser = argparse.ArgumentParser(description='Summarize BLAST search resutls.')
-parser.add_argument('organism_list', default='proteome_list.tsv', help='List of organisms in tsv format')
+parser.add_argument('organism_list', help='List of organisms in tsv format')
 parser.add_argument('-i', '--input', default='blast', help='BLAST results directory')
 parser.add_argument('-o', '--outdir', default='matrix', help='Output directory')
 args = parser.parse_args()
