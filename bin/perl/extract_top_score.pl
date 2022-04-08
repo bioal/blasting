9b6bffed791feb01a2e966ce4de799ebdbbcfe57
Hirokazu Chiba <hchiba1@gmail.com>
2022-04-08 10:07:57 +0900

    Add perl/extract_top_score.pl

---
+++ bin/perl/extract_top_score.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl -w
+use strict;
+use File::Basename;
+use Getopt::Std;
+my $PROGRAM = basename $0;
+my $USAGE=
+"Usage: $PROGRAM
+";
+
+my %OPT;
+getopts('', \%OPT);
+
+!@ARGV && -t and die $USAGE;
+my $CURRENT_QUERY;
+my $TOP_SCORE;
+while (<>) {
+    chomp;
+    my @f = split(/\t/, $_);
+    if (@f < 12) {
+        die;
+    }
+    my $query = $f[0];
+    my $hit = $f[1];
+    my $score = $f[11];
+    if (! $CURRENT_QUERY or
+        $query ne $CURRENT_QUERY or
+        $query eq $CURRENT_QUERY and $score eq $TOP_SCORE
+        ) {
+        print $query, "\t", $hit, "\t", $score, "\n";
+        $TOP_SCORE = $score;
+    }
+    $CURRENT_QUERY = $query;
+}
