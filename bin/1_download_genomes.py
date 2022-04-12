7b06108eb4a1e70d3c410cf1cd0c28cb64580210
Hirokazu Chiba <hchiba1@gmail.com>
2022-04-13 06:37:52 +0900

    Refactor 1_download_genomes.py

--- bin/1_download_genomes.py
+++ bin/1_download_genomes.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import os
+import re
 import argparse
 import subprocess
 from classes.FtpCli import FtpCli
-from classes.GenomeDownloader import GenomeDownloader
 
 parser = argparse.ArgumentParser(description='Download genomes from NCBI, according to the species list in tsv format.')
 parser.add_argument('species_list', help='Species list in tsv format')
@@ -12,8 +12,9 @@ parser.add_argument('-o', '--outdir', default='data', help='Output directory')
 parser.add_argument('-d', '--debug', action='store_true', help='For debug: do not download the genomes')
 args = parser.parse_args()
 
-if not os.path.exists(args.outdir):
-    os.makedirs(args.outdir)
+out_dir = f'{args.outdir}/genomes'
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
 
 ftp = FtpCli('ftp.ncbi.nlm.nih.gov')
 ftp_dir = '/genomes/ASSEMBLY_REPORTS/'
@@ -25,5 +26,30 @@ ftp.close()
 genomes_found = f'{args.outdir}/genomes_found.tsv'
 subprocess.run(f'./bin/perl/find_gcf_file.pl {args.species_list} {summary_file} | sort -n > {genomes_found}', shell=True)
 
-downloader = GenomeDownloader(args.outdir + '/genome', args.cores)
-downloader.sync(genomes_found, args.outdir + '/genome.tsv', args.outdir + '/genome.err', args.debug)
+fp = open(genomes_found, 'r', encoding='UTF-8')
+fp_out = open(f'{args.outdir}/genomes_downloaded.tsv', 'w')
+for line in fp:
+    line = line.rstrip()
+    fields = line.split('\t')
+    id = fields[0]
+    url = fields[20]
+    server = url.replace('ftp://', '')
+    server = url.replace('https://', '')
+    index = server.find('/')
+    path = server[index:]
+    server = server[0:index]
+    index = path.rfind('/')
+    name = path[index + 1:]
+    if name.endswith('gz'):
+        gz_file_name = name
+        gz_file_path = path
+    else:
+        gz_file_name = name + '_protein.faa.gz'
+        gz_file_path = f'{path}/{gz_file_name}'
+    outfile = out_dir + '/' + gz_file_name
+    ftp = FtpCli(server)
+    ftp.sync(gz_file_path, outfile)
+    ftp.close()
+    print(id + '\t' + re.sub(r'.gz', '', outfile), file=fp_out, flush=True)
+fp.close()
+fp_out.close()
