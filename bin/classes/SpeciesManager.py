fe3f77b80012fbec81ea1549850f79c7ef0555f6
Hirokazu Chiba <hchiba1@gmail.com>
2022-04-13 05:45:41 +0900

    Refactor SpeciesManager.py

--- bin/classes/SpeciesManager.py
+++ bin/classes/SpeciesManager.py
@@ -3,19 +3,18 @@ import re
 
 class SpeciesManager:
     def __init__(self, species_list):
-        # [0] no
-        # [1] taxid
-        # [2] name
         self.ids, self.taxids, self.species = {}, {}, {}
         fp = open(species_list, 'r')
         for line in fp:
             line = line.rstrip('\r\n')
             fields = line.split('\t')
-            # skip header line
-            if re.match(r'^[1-9][0-9]*$', fields[0]):
-                self.ids[fields[0]] = line
-                if len(fields) > 2:
-                    self.species[fields[2]] = fields[0]
-                if len(fields) > 1:
-                    self.taxids[fields[1]] = fields[0]
+            if len(fields) < 3:
+                continue
+            no = fields[0]
+            taxid = fields[1]
+            name = fields[2]
+            if re.match(r'^[1-9][0-9]*$', no):
+                self.ids[no] = line
+                self.taxids[taxid] = no
+                self.species[name] = no
         fp.close()
