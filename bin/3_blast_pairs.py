bf067ad0e28ed1533349ef5918aad6da45408bb5
Hirokazu Chiba <hchiba1@gmail.com>
2022-04-13 23:19:25 +0900

    Modify 3_blast_pairs.py

--- bin/3_blast_pairs.py
+++ bin/3_blast_pairs.py
@@ -3,11 +3,13 @@ import argparse
 from classes.BlastManager import BlastManager
 
 parser = argparse.ArgumentParser(description='BLAST search for pairs of the specified organisms.')
-parser.add_argument('organism_list', help='List of organisms in tsv format')
+parser.add_argument('genomes_downloaded', help='List of genomes downloaded')
+parser.add_argument('pairs_file', help='Pairs of organisms to be compared')
 parser.add_argument('-n', '--cores', required=True, type=int, help='Number of CPU cores to be used for BLAST')
 parser.add_argument('-p', '--program', default='blastp', help='Path to blastp command (default: blastp)')
+parser.add_argument('--dbdir', default='data/blastdb', help='blastdb directory')
 parser.add_argument('-o', '--outdir', default='blast.out', help='Output directory')
 args = parser.parse_args()
 
-manager = BlastManager(args.program, args.cores, args.organism_list, 'data/blastdb', args.outdir)
-manager.search()
+manager = BlastManager(args.program, args.cores, args.genomes_downloaded, args.dbdir, args.outdir)
+manager.exec_pairs(args.pairs_file)
