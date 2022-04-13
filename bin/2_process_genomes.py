6158294b0bfa3f391caaf3f3d1d9ecc84b436cc6
Hirokazu Chiba <hchiba1@gmail.com>
2022-04-13 17:27:12 +0900

    Update bin/2_process_genomes.py

--- bin/2_process_genomes.py
+++ bin/2_process_genomes.py
@@ -5,7 +5,8 @@ from classes.DataManager import DataManager
 parser = argparse.ArgumentParser(description='Preprocess genome data for BLAST search.')
 parser.add_argument('genome_list', help='List of downloaded genomes in tsv format')
 parser.add_argument('-p', '--program', default='makeblastdb', help='Path to makeblsastdb command (default: makeblastdb)')
+parser.add_argument('-o', '--outdir', default='data', help='Output directory')
 args = parser.parse_args()
 
-manager = DataManager(args.genome_list, args.program, 'data/genes', 'data/blastdb')
+manager = DataManager(args.genome_list, args.program, f'{args.outdir}/genes', f'{args.outdir}/blastdb')
 manager.preprocess()
