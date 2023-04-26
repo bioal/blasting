7757fb438f330a8e9f76e8be4a267494279bdfa4
Hirokazu Chiba <hchiba1@gmail.com>
2023-04-26 16:46:38 +0900

    Add bin/python/summarize_scores_parallel.py --outdir option

--- bin/python/summarize_scores_parallel.py
+++ bin/python/summarize_scores_parallel.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
+import argparse
 import os
 import glob
 import subprocess
 from concurrent.futures import ThreadPoolExecutor
 
-# 入力フォルダのパス
-output_path = './out'
-dbm_path = '/home/chiba/github/bioal/blasting/examples/gene_descr.dbm'
+parser = argparse.ArgumentParser(description='Summarize scores of each gene in parallel.')
+parser.add_argument('-o', '--outdir', default='summarized_score', help='Output directory')
+args = parser.parse_args()
 
+if not os.path.exists(args.outdir):
+    os.makedirs(args.outdir)
+
+# 入力フォルダのパス
+dbm_path = '/home/chiba/github/bioal/blasting/examples/gene_descr.dbm'
 top_score_path = '/mnt/share/chiba/orth/blasting.homologene.2022-04/top_score_to_human'
 
 # ファイルパスのリストを取得
@@ -17,7 +23,7 @@ file_paths = glob.glob('*.out')
 def run_command(file_path):
     # bashコマンドを実行
     file_name, file_ext = os.path.splitext(os.path.basename(file_path))
-    command = f'summarize_scores_with_descr.pl -v -t {top_score_path} -d {dbm_path} {file_path} > {os.path.join(output_path, file_name)}.scores.txt'
+    command = f'summarize_scores_with_descr.pl -v -t {top_score_path} -d {dbm_path} {file_path} > {os.path.join(args.outdir, file_name)}.scores.txt'
     print(command)
     subprocess.run(['bash', '-c', command])
 
