a215742735e1c58d2245be0d6bbacbca1ce0bcac
Hirokazu Chiba <hchiba1@gmail.com>
2023-04-28 07:36:14 +0900

    Refactor bin/python/summarize_scores_parallel.py subprocess.run

--- bin/python/summarize_scores_parallel.py
+++ bin/python/summarize_scores_parallel.py
@@ -19,8 +19,7 @@ file_paths = glob.glob('*.out')
 def run_command(file_path):
     file_name, file_ext = os.path.splitext(os.path.basename(file_path))
     out_file_path = os.path.join(args.outdir, file_name)
-    command = f'/home/chiba/github/bioal/blasting/bin/perl/summarize_scores_with_descr.pl -v -t {top_score_path} -d {dbm_path} {file_path} > {out_file_path}.scores.txt 2> {out_file_path}.scores.err'
-    subprocess.run(command, shell=True)
+    subprocess.run(f'/home/chiba/github/bioal/blasting/bin/perl/summarize_scores_with_descr.pl -v -t {top_score_path} -d {dbm_path} {file_path} > {out_file_path}.scores.txt 2> {out_file_path}.scores.err', shell=True)
 
 with ThreadPoolExecutor(max_workers=96) as executor:
     executor.map(run_command, file_paths)
