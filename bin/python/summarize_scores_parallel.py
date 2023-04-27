041f68aff65d31a4bc23cfcd234382358e813df3
Hirokazu Chiba <hchiba1@gmail.com>
2023-04-27 14:55:46 +0900

    Simplify bin/python/summarize_scores_parallel.py

--- bin/python/summarize_scores_parallel.py
+++ bin/python/summarize_scores_parallel.py
@@ -12,22 +12,15 @@ args = parser.parse_args()
 if not os.path.exists(args.outdir):
     os.makedirs(args.outdir)
 
-# 入力フォルダのパス
 dbm_path = '/home/chiba/github/bioal/blasting/examples/seq_descr.dbm'
 top_score_path = '/mnt/share/chiba/orth/blasting.homologene.2022-04/top_score_to_human'
-
-# ファイルパスのリストを取得
 file_paths = glob.glob('*.out')
 
-# コマンドの実行関数
 def run_command(file_path):
-    # bashコマンドを実行
     file_name, file_ext = os.path.splitext(os.path.basename(file_path))
     out_file_path = os.path.join(args.outdir, file_name)
     command = f'/home/chiba/github/bioal/blasting/bin/perl/summarize_scores_with_descr.pl -v -t {top_score_path} -d {dbm_path} {file_path} > {out_file_path}.scores.txt 2> {out_file_path}.scores.err'
-    print(command)
     subprocess.run(['bash', '-c', command])
 
-# マルチスレッドでコマンドを実行
 with ThreadPoolExecutor(max_workers=96) as executor:
     executor.map(run_command, file_paths)
