bba485c8123d0eaa0618e7f6e9a513f4b6059f6b
Hirokazu Chiba <hchiba1@gmail.com>
2023-04-28 09:47:53 +0900

    Refactor bin/python/summarize_scores_parallel.py

--- bin/python/summarize_scores_parallel.py
+++ bin/python/summarize_scores_parallel.py
@@ -12,14 +12,14 @@ args = parser.parse_args()
 if not os.path.exists(args.outdir):
     os.makedirs(args.outdir)
 
-dbm_path = '/home/chiba/github/bioal/blasting/examples/seq_descr.dbm'
-top_score_path = '/mnt/share/chiba/orth/blasting.homologene.2022-04/top_score_to_human'
-file_paths = glob.glob('*.out')
+def summarize(file_path):
+    dbm_path = '/home/chiba/github/bioal/blasting/examples/seq_descr.dbm'
+    top_score_to_human = '/mnt/share/chiba/orth/blasting.homologene.2022-04/top_score_to_human'
+    file_name = os.path.basename(file_path)
+    name, ext = os.path.splitext(file_name)
+    out_path = os.path.join(args.outdir, name)
+    subprocess.run(f'/home/chiba/github/bioal/blasting/bin/perl/summarize_scores_with_descr.pl -v -t {top_score_to_human} -d {dbm_path} {file_path} > {out_path}.scores.txt 2> {out_path}.scores.err', shell=True)
 
-def run_command(file_path):
-    file_name, file_ext = os.path.splitext(os.path.basename(file_path))
-    out_file_path = os.path.join(args.outdir, file_name)
-    subprocess.run(f'/home/chiba/github/bioal/blasting/bin/perl/summarize_scores_with_descr.pl -v -t {top_score_path} -d {dbm_path} {file_path} > {out_file_path}.scores.txt 2> {out_file_path}.scores.err', shell=True)
-
-with ThreadPoolExecutor(max_workers=96) as executor:
-    executor.map(run_command, file_paths)
+with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
+    files = glob.glob('*.out')
+    executor.map(summarize, files)
